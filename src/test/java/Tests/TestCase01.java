59bfe6dc95fa4bd9f757b4df143fd9a81025d279
NarasayyaShetty <NarasayyaShetty497@gmail.com>
2025-05-03 00:03:11 +0530

    New Commit

--- src/test/java/Tests/TestCase01.java
+++ src/test/java/Tests/TestCase01.java
@@ -28,8 +28,9 @@ public class TestCase01 {
     }
 
     @Test(description="testcase o1")
-    public void testcase01(){
+    public void testcase01() throws InterruptedException {
         Assert.assertTrue(homePage.isHomePage(),"Unable to navigate to homepage");
+        Thread.sleep(2000);
         homePage.clickRegisterButton();
         Assert.assertTrue(registerPage.isRegisterPage(),"Unable to navigate to register page");
 
